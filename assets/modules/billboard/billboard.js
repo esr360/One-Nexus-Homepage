e647adf3ab3f0f62bf5a5a65f5ecaf918dd6e9fe
esr360 <esr360@live.com>
2016-12-07 00:35:32 +0000

    various updates

---
+++ assets/modules/billboard/billboard.js
@@ -0,0 +1,48 @@
+(function ($) {
+
+    /**
+     * Billboard
+     * 
+     * @access public
+     * @author [@esr360](http://twitter.com/esr360)
+     * @param {object} custom - where custom config will be passed
+     * 
+     * @example
+     *     $('.billboard').billboard({
+     *         parallax: false
+     *     });
+     */
+    
+    $.fn._billboard = $.fn.billboard;
+
+    $.fn.billboard = function(custom) {
+        
+        // Options
+        var options = $.extend({
+            parallax: true
+        }, custom);
+
+        var billboard = $(this);
+
+        billboard._billboard(options);
+
+        if (options.parallax) {
+            $(window).on('scroll', function() {
+                var scrollTop = $(this).scrollTop();
+                var opacity = 1 - scrollTop * 0.003;
+
+                billboard.css({
+                    'background-position-y': scrollTop/-1.8 + 'px'
+                });
+
+                billboard.find('.billboard_wrapper').css({
+                    'transform': 'translateY(' + scrollTop/2 + 'px)',
+                    'opacity': opacity
+                });
+            });
+        }
+
+    }
+
+
+}(jQuery));
